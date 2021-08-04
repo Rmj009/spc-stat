12130630968cb9102cde23707ee770f58f2130b6
劉 宏毅 <quanta10811053@gmail.com>
2021-08-04 10:57:19 +0800

    recommit

--- errors.py
+++ errors.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
-from app import app
+# import request
+# from flask import request
+# from app import app
 db = SQLAlchemy()
 
 #-------ERROR Handling----------
@@ -9,19 +11,33 @@ db = SQLAlchemy()
 Returns:
 500 and msg which caused problems
 """
+# @app.route('/oauth/authorize', methods=['GET', 'POST'])
+# @require_login
+# @oauth.authorize_handler
+# def authorize(*args, **kwargs):
+#     if request.method == 'GET':
+#         client_id = kwargs.get('client_id')
+#         client = Client.query.filter_by(client_id=client_id).first()
+#         kwargs['client'] = client
+#         return render_template('oauthorize.html', **kwargs)
+
+#     confirm = request.form.get('confirm', 'no')
+#     return confirm == 'yes'
+
+
 # class PassGateway:
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'),error, 404
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html'),error, 404
 
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'),error, 500
+# @app.errorhandler(500)
+# def internal_error(error):
+#     db.session.rollback()
+#     return render_template('500.html'),error, 500
 
-@app.teardown_appcontext
-def shotdown_session(error):
-    print ("@app.teardown_appcontext: shotdown_session()")
-    db.session.remove()
-    db.session.rollback()
-    return error, 500
+# @app.teardown_appcontext
+# def shotdown_session(error):
+#     print ("@app.teardown_appcontext: shotdown_session()")
+#     db.session.remove()
+#     db.session.rollback()
+#     return error, 500

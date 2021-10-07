df56292c4cc1daed18955726591d5c9633667e46
Paul Liu <quanta10811053@gmail.com>
2021-10-07 15:29:59 +0800

    revised clustering rule

--- utils/gauge.py
+++ utils/gauge.py
@@ -18,11 +18,22 @@ class Gauge():
     
     def __repr__(self):
         return f"SpcMeasurePointConfigUUID:{self.points,self.LSLlst,self.USLlst,self.measureAmount,self.stdValue}."
+    def wipe_out_nan(pts):
+        return [i.strip() for i in pts if bool(i and not i.isspace())]
+
+    def sliding_chunker(original, segment_len, slide_len):
+        chunks = []
+        for pos in range(0, len(original), slide_len):
+            chunk = np.copy(original[pos:pos + segment_len])
+            if len(chunk) != segment_len:
+                continue
+            chunks.append(chunk)
+        return chunks
+
 
     def stats(points,LSLlst,USLlst,measureAmount,stdValue): #good,defect,
         points = [i for i in (points.split(','))]
-        points = pd.DataFrame(points)
-        # print('print(::::)\n',points)
+        # points = pd.DataFrame(points)
         # print('print(points)',points)
         # points = [ float(i) for i in enumerate(points.split(','))]
         # [dict([i, int(x)] for i, x in b.items()) for b in list]
@@ -32,10 +43,10 @@ class Gauge():
         specificationcols = ["LSLlst","USLlst","measureAmount","stdValue"]
         df = pd.DataFrame(dict(zip(specificationcols, specification)),index= [0])
         # df = np.array([LSLlst,USLlst,measureAmount,stdValue]).astype(float)#,index=integer_array) 
-        print('dfdfdf\n',df)
-        Target = df['stdValue']
-        LSLlst = df['LSLlst']
-        USLlst = df['USLlst']
+        
+        Target = df['stdValue'].astype('float64')
+        LSLlst = df['LSLlst'].astype('float64')
+        USLlst = df['USLlst'].astype('float64')
         countgooddefect = []
         measureAmount = int(df['measureAmount'])
         # print('measureAmount',measureAmount)
@@ -45,11 +56,15 @@ class Gauge():
         UCL = (USL + Target)/2
         rangespec = USL - LSL
         totalNum = len(points)
-        print('::::: \n',totalNum)
+        # pointslst = [list() for i in map(str,range(len(points)//measureAmount))]
+        print('len(points)//measureAmount',len(points)//measureAmount)
+        # points = np.array_split(points, len(points)//measureAmount)
+        
+        points = Gauge.sliding_chunker(points, segment_len = measureAmount, slide_len = len(points)//measureAmount )
+        print('::::: \n',points)
         print(':::::: \n')
-        pointslst = [list() for i in map(str,range(len(points)//measureAmount))]
-        nonenum = []
-        points = np.array_split(points, len(points)//measureAmount)
+        # print(pointslst)
+        print('------------------',type(USL),LSL,LCL,UCL)
         # print('aa \n',points[0])
         # trim = points[1]
         # # pp = [item.apply(lambda x: x.str.strip()) for item in trim] #item.strip(' ') x.str.strip(' ')
@@ -59,45 +74,51 @@ class Gauge():
         #         print('F')
             # nonenum.append(item.strip(' '))
         # print('strip\n', pp)
-        
-        # print('rrr', nonenum)
-        for item in points: #for item in item:
-            if (bool(item and not item.isspace()) == False): # check the empty , None or blank
-                nonenum += 1
-                print('vvvv',v.items())
-                if (v > USL) or (v < LSL) :
-                    countgooddefect.append(1) # add defect
-                else:
-                    countgooddefect.append(0) # add good
-            else:
-                # flags = 0
-                # pointslst[flags].append(item)
-                print('series',item)
+        pointslst = [[i.strip() for i in lst if bool(i and not i.isspace())] for lst in points] # iterate 1st_outer list, 2nd_inner list
+        pointslst = [[float(i) for i in lst ] for lst in pointslst]
+        print('///point_into_subgroup////', pointslst)
+        # print('///point_into_subgroup////', pointslst)
+        # print('aa',aa)
+            # for i in item:
+            #     if (bool(i and not i.isspace()) == False): # check the empty , None or blank
+            #         nonenum += 1
+            #         print('vvvv', type(i))
+            #     else:
+            #         flags = 0
+            #         pointslst[flags].append(i)
+            #         print('series',i)
         
         # print('len(points)',points)
         # subgroup1_num = len(points) - nonenum
-        # print(pointslst)
-
-        # for k,v in points:
-        #     if (v > USL) or (v < LSL) :
-        #         defect.append(1)
-        #     else:
-        #         good.append(1)
+        print('gooddefectcount')
+        for lst in pointslst:
+            for item in lst:
+                if (item > USL.item() or item < LSL.item()) :
+                    # print('1',item)
+                    countgooddefect.append(1)
+                else:
+                    # print('0',item)
+                    countgooddefect.append(0)
         # points = points.str.rsplit( n = measureAmount)
-        # print('///////', pointslst)
+        
         goodRate = countgooddefect.count(0)/ totalNum
+        print('///////', goodRate)
+        cpkarrMEAN = [np.mean(i) for i in pointslst]
+        sigmaCpk = np.std(cpkarrMEAN,ddof=1) #pd.std() >>> //(n)
+        print('///////', cpkarrMEAN,sigmaCpk)
 
-        ngroup = len(points)/measureAmount
-        if (ngroup.is_integer() == False):
-            cpkarr = np.array_split(points[::-1],len(points)//measureAmount) #revserve to split coz the array_split method
-            cpkarrMEAN = [np.mean(i) for i in cpkarr]
-            sigmaCpk = np.std(cpkarrMEAN,ddof=1) #pd.std() >>> //(n)
-        else:
-            cpkarr = np.array_split(points,ngroup)
-            cpkarrMEAN = [np.mean(i) for i in cpkarr]
-            sigmaCpk = np.std(cpkarrMEAN,ddof=1) # numpy standard deviation >>> //(n-1)
-        cp_mean = np.mean(points)
-        sigmaPpk = np.std(points,ddof=1)
+        # ngroup = len(points)/measureAmount
+        # if (ngroup.is_integer() == False):
+        #     cpkarr = np.array_split(points[::-1],len(points)//measureAmount) #revserve to split coz the array_split method
+        #     cpkarrMEAN = [np.mean(i) for i in cpkarr]
+        #     sigmaCpk = np.std(cpkarrMEAN,ddof=1) #pd.std() >>> //(n)
+        # else:
+        #     cpkarr = np.array_split(points,ngroup)
+        #     cpkarrMEAN = [np.mean(i) for i in cpkarr]
+        #     sigmaCpk = np.std(cpkarrMEAN,ddof=1) # numpy standard deviation >>> //(n-1)
+        flatten_pointslst = [val for sublist in pointslst for val in sublist]
+        cp_mean = np.mean(flatten_pointslst)
+        sigmaPpk = np.std(flatten_pointslst,ddof=1)
  
         assert sigmaPpk != 0
         assert sigmaCpk != 0
@@ -113,9 +134,10 @@ class Gauge():
         Ppl = (cp_mean - LCL) / (sigmaPpk*3)
         Ppk = np.min([Ppu,Ppl]) 
         # capability ratio
-        CPR = sigmaPpk, sigmaCpk, len(good), totalNum, goodRate ,USL,LSL,UCL,LCL,cp_mean,Target ,rangespec, Cpu, Cpl, Cp, Ck, Cpk, Ppk
+        CPR = sigmaPpk, sigmaCpk, countgooddefect.count(0), totalNum, goodRate ,USL.item(),LSL.item(),UCL.item(),LCL.item(),cp_mean,Target.item() ,rangespec.item(), Cpu.item(), Cpl.item(), Cp.item(), Ck.item(), Cpk, Ppk
         keys = ["sigma","groupSigma","good","totalNum","goodRate","USL","LSL","UCL","LCL","overallMean","target","range","Cpu","Cpl","Cp","Ck","Cpk","Ppk"]
         capability = dict(zip(keys, CPR))
+        print('capability',capability)
         ### Reference :https://en.wikipedia.org/wiki/Process_performance_index
         return capability # total 17 + 2
     
